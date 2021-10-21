96453a27877d7ab6c800ff6324e6533fb378394f
unknown <calwong42991@gmail.com>
2021-10-20 21:51:54 -0700

    maxSubArray.py completed

--- leet-code/python/maxSubArray.py
+++ leet-code/python/maxSubArray.py
@@ -5,14 +5,25 @@ class Solution(object):
         :rtype: int
         """
 
+        maxSub = nums[0]
+        curSum = 0
+        
+        for n in nums:
+            #print(n)
+            if curSum < 0:
+                curSum = 0
+            curSum += n
+
+            maxSub = max(curSum, maxSub)
+        return maxSub
 
 
         
 
 nums = {
     "list_1": [-2,1,-3,4,-1,2,1,-5,4],
-    "list_2": [1],
-    "list_2": [5,4,-1,7,8]
+    "list_2": [-2,1],
+    "list_3": [5,4,-1,7,8]
 }
 
 s = Solution()
