a9afc3a46211ee85ce352d50d0657bf629dfc4b4
unknown <calwong42991@gmail.com>
2021-10-25 19:00:24 -0700

    search.py optimized w/ binary search complete

--- leet-code/python/Array/search.py
+++ leet-code/python/Array/search.py
@@ -12,6 +12,33 @@ class Solution(object):
 
         return -1
 
+    def search_Optimization(self, nums, target):
+
+        output = self.binarySearch(nums, target)
+
+
+        return output
+
+    def binarySearch(self, arr, target):
+
+        start = 0
+        end = len(arr) - 1
+
+        while start <= end:
+            midpoint = (start + end)//2
+            if arr[midpoint] == target:
+                return midpoint
+            else:
+                if target < arr[midpoint]:
+                    end = midpoint - 1
+                else: 
+                    start = midpoint +1
+
+        return midpoint
+
+
+
+
 nums = {
     "list_1": [4,5,6,7,0,1,2],
     "list_2": [4,5,6,7,0,1,2],
@@ -26,5 +53,5 @@ target = {
 
 s = Solution()
 print(s.search(nums['list_1'], target['target_1'])) #4
-#print(s.search(nums['list_2'], target['target_2'])) #-1
-#print(s.search(nums['list_3'], target['target_3'])) #-1
+print(s.search(nums['list_2'], target['target_2'])) #-1
+print(s.search(nums['list_3'], target['target_3'])) #-1
