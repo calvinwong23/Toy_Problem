8bac8e815db7f7e5600ca7954e13f560e7b734c0
unknown <calwong42991@gmail.com>
2021-10-26 21:06:54 -0700

    maxArea.py completed

--- leet-code/python/Array/maxArea.py
+++ leet-code/python/Array/maxArea.py
@@ -4,6 +4,38 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
+        # length of input array
+        size = len(height)
+        
+        # two pointers, left init as 0, right init as size-1
+        left, right = 0, size-1
+        
+        # maximal width between leftmost stick and rightmost stick
+        max_width = size - 1
+        
+        # area also known as the amount of water
+        area = 0
+        
+		# trade-off between width and height
+        # scan each possible width and compute maximal area
+        for width in range(max_width, 0, -1):
+            
+            if height[left] < height[right]:
+                # the height of lefthand side is shorter
+                area = max(area, width * height[left] )
+                
+                # update left index to righthand side
+                left += 1
+                
+            else:
+                # the height of righthand side is shorter
+                area = max(area, width * height[right] )
+                
+                # update right index to lefthand side
+                right -= 1
+                
+        return area
+                
 
 height = {
     "list_1": [1,8,6,2,5,4,8,3,7],
