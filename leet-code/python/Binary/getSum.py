0d3964f08fe349451c663308f52676df4625c780
calwong23 <calvinwong.tse@gmail.com>
2021-10-29 19:25:54 -0700

    getSum.py completed

--- leet-code/python/Binary/getSum.py
+++ leet-code/python/Binary/getSum.py
@@ -5,6 +5,17 @@ class Solution(object):
         :type b: int
         :rtype: int
         """
+        if a == -b:
+            return 0
+        if abs(a) > abs(b):
+            a, b = b, a
+        if a < 0:
+            return -self.getSum(-a, -b)
+        while b:
+            c = a & b
+            a ^= b
+            b = c << 1
+        return a
 
 s = Solution()
 print(s.getSum(1,2)) #3
