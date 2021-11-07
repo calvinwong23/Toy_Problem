9f0483605cc060d311f8425119d2ef703f0acb35
calwong23 <calvinwong.tse@gmail.com>
2021-11-06 18:18:49 -0700

    reverseBits.py created

---
+++ leet-code/python/Binary/reverseBits.py
@@ -0,0 +1,21 @@
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+
+        
+        return n
+
+
+nums = {
+    "n_1": "00000010100101000001111010011100",
+    "n_2": 11111111111111111111111111111101
+},
+
+s = Solution()
+print(s.reverseBits(nums['n_1'])) # 964176192 (00111001011110000010100101000000)
+#print(s.reverseBits(nums['n_2'])) # 3221225471 (10111111111111111111111111111111)
+
+
+
+
