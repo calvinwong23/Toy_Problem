b8bd5d943755b12879d76ad0e6dd54d0711f4ee5
calwong23 <calvinwong.tse@gmail.com>
2021-11-07 18:00:56 -0800

    reverseBits.py completed

--- leet-code/python/Binary/reverseBits.py
+++ leet-code/python/Binary/reverseBits.py
@@ -2,13 +2,16 @@ class Solution:
     # @param n, an integer
     # @return an integer
     def reverseBits(self, n):
-
-        
-        return n
+        res    = 0  
+        for i in range(32):
+            if n&1:
+                res += 1 << (31-i)
+            n >>= 1
+        return res
 
 
 nums = {
-    "n_1": "00000010100101000001111010011100",
+    "n_1": 00000010100101000001111010011100,
     "n_2": 11111111111111111111111111111101
 },
 
