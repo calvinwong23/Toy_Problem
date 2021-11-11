e86a3b525822dcbdc6a8c4df045095b0bb3948dd
calwong23 <calvinwong.tse@gmail.com>
2021-11-11 00:23:03 -0800

    coinChange.py completed

--- leet-code/python/Dynamic Programming/coinChange.py
+++ leet-code/python/Dynamic Programming/coinChange.py
@@ -5,4 +5,12 @@ class Solution(object):
         :type amount: int
         :rtype: int
         """
+        if amount == 0:
+            return 0
         
+        dp = [0]+[math.inf]*amount
+        coins = sorted(coins)
+        for i in range(1,amount+1):
+            if i >= coins[0]:
+                dp[i] = 1+min(dp[i-j] for j in coins if j <= i)
+        return dp[-1] if dp[-1]<=amount else -1
