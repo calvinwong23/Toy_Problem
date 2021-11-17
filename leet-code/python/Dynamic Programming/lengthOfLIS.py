fa1f2373c50abcbf38e0cf70e83d05bbd18fdbcf
calwong23 <calvinwong.tse@gmail.com>
2021-11-16 17:59:18 -0800

    lengthOfLIS.py complete

--- leet-code/python/Dynamic Programming/lengthOfLIS.py
+++ leet-code/python/Dynamic Programming/lengthOfLIS.py
@@ -4,4 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        if not nums: 
+            return 0
+
+        seql = [0]*len(nums)
+        seql[0] = 1
+        maxl = 1
+        for i in range(1,len(nums)):
+            this_maxl = 1
+            for j in range(0, i):
+                if nums[j]<nums[i]:
+                    if seql[j]+1 > this_maxl:
+                        this_maxl = seql[j]+1
+            seql[i] = this_maxl
+            if seql[i] > maxl:
+                maxl = seql[i]
+        return maxl
         
