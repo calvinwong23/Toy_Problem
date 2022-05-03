ed06a995c3c724d780dbc6fddf4c640e081747dc
Calvin Wong <calvinwong.tse@gmail.com>
2022-05-02 19:00:11 -0700

    longest common prefix complete

--- leet-code/python/Leet Code/longestCommonPrefix.py
+++ leet-code/python/Leet Code/longestCommonPrefix.py
@@ -5,4 +5,18 @@ class Solution(object):
         :rtype: str
         """
 
+        if not strs:
+            return ''
+        shortest = min(strs, key = len)
+        for i in range(len(shortest)):
+            if sum(1 for s in strs if shortest[i] != s[i]) > 0:
+                return shortest[:i]
+            
+        return shortest
         
+
+s = Solution()
+print(s.longestCommonPrefix(["flower","flow","flight"]))
+#print(s.checkPalindrome(a,1,1))
+#print(s.checkPalindrome(a,0,0))
+
